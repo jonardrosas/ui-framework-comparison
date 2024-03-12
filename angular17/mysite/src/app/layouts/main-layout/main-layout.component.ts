a6c306d6d9f00e6fb631bec22ec347e780ef50c8
Jonard <jonardrosas@gmail.com>
2024-03-12 18:30:03 +0800

    add angular theming

--- angular17/mysite/src/app/layouts/main-layout/main-layout.component.ts
+++ angular17/mysite/src/app/layouts/main-layout/main-layout.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterModule, RouterOutlet } from '@angular/router';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-main-layout',
   standalone: true,
-  imports: [RouterOutlet, RouterModule],
+  imports: [RouterOutlet, RouterModule, MatSidenavModule, MatToolbarModule, MatButtonModule, MatIconModule],
   templateUrl: './main-layout.component.html',
   styleUrl: './main-layout.component.scss'
 })
